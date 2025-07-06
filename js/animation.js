f9873023f779fb15b5821b22dacce64d7a361811
Chirayu Sahu <chirayuajaysahu01@gmail.com>
2025-07-06 19:27:38 +0530

    fix: animation ease changed

--- js/animation.js
+++ js/animation.js
@@ -1,19 +1,25 @@
 const logo = document.querySelector('#logo');
 const navbar = document.querySelector('#navbar');
 const navText = document.querySelectorAll('.nav-text');
+const logoText = document.querySelectorAll('.logo-text');
+const heroVideo = document.querySelector('#heroVideo');
 
 gsap.registerPlugin(ScrollTrigger);
 
 const timeline = gsap.timeline();
 
-timeline.fromTo(logo, {
-    opacity: 0,
-    y: 20,
+
+timeline.fromTo(logoText, {
+    opacity: 1,
+    y: -1000,
 }, {
     opacity: 1,
     y: 0,
     duration: 0.5,
+    stagger: 0.1,
+    ease: 'ease.inOut',
 });
+
 
 timeline.fromTo(navText, {
     opacity: 0,
@@ -25,20 +31,29 @@ timeline.fromTo(navText, {
     stagger: 0.1,
 });
 
+gsap.fromTo(heroVideo, {
+    opacity: 0,
+}, {
+    opacity: 1,
+    duration: 0.5,
+    delay: 0.5,
+    ease: 'ease.inOut',
+});
+
 gsap.fromTo(logo, 
     { 
         scale: 1,
         y: 0,
      },
     { 
-        scale: 0.1,
-        y: -150,
+        scale: 0.17,
+        y: -120,
         duration: 1,
-        ease: 'power2.inOut',
+        ease: 'linear',
         scrollTrigger: {
             trigger: logo,
             start: 'top 0%',
-            end: 'bottom 20%',
+            end: 'bottom 10%',
             scrub: true,
         }
     }
@@ -48,8 +63,8 @@ gsap.fromTo(navbar, {
     y: 10,
 }, {
     y: -358,
-    duration: 4,
-    ease: 'power2.inOut',
+    duration: 1,
+    ease: 'linear',
     scrollTrigger: {
         trigger: navbar,
         start: 'top 40%',
